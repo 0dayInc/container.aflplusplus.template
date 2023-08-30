9a8b456f9e7a91b85379070d1785bbd1477604c7
Jacob Hoopes <jake.hoopes@gmail.com>
2023-08-30 16:33:38 -0600

    A lot of #bugfixes

--- TARGET/instrument_target.sh
+++ TARGET/instrument_target.sh
@@ -1,11 +1,13 @@
 # #!/bin/bash --login
 target_source_name="${1}"
-docker_repo_root='/opt/container.aflplusplus.template'
-target_repo="${docker_repo_root}/TARGET_SRC/${target_source_name}"
-aflplusplus_source_root='/AFLplusplus'
+container_afl_template_path='/opt/container.aflplusplus.template'
+intrumentation_globals="${container_afl_template_path}/TARGET/instrumentation_globals.sh"
+source $intrumentation_globals
+target_repo="${container_afl_template_path}/TARGET_SRC/${target_source_name}"
 
 # Provide an opportunity to troubleshoot the container
 bash --login -c "
+  printf '\n\n\n';
   echo '#--------------------------------------------------------#';
   echo '| Welcome to the AFL++ Container...                      |';
   echo '| Feel Free to Browse the Filesystem, Troubleshoot, etc. |';
@@ -23,8 +25,9 @@ cd ${target_repo} && CC=$preferred_afl CXX=$preferred_aflplusplus RANLIB=$prefer
 cd ${target_repo} && CC=$preferred_afl CXX=$preferred_aflplusplus RANLIB=$preferred_afl_ranlib AR=$preferred_afl_ar NM=$preferred_afl_nm make
 cd ${target_repo} && make install
 
-echo 'INFO: afl-fuzz will begin in 10 seconds'
+printf "\nINFO: afl-fuzz will begin in 10 seconds"
 for i in {1..10}; do
   printf '.'
   sleep 1
 done
+printf "\n"
