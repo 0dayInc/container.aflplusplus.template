ef886c31b4ee23bef77bc662919d74ef208af363
Jacob Hoopes <jake.hoopes@gmail.com>
2023-09-05 10:49:36 -0600

    Include preeny while initializing afl++ container and load up AFL_PRELOAD with useful .so files provided by preeny

--- AFLplusplus_template.sh
+++ AFLplusplus_template.sh
@@ -218,9 +218,9 @@ case $afl_mode in
     # BIND MOUNTS TO THE DOCKER CONTAINER
     docker_name="aflplusplus.${this_session_rand}"
     # tmux new -s "afl_M_$this_session_rand" \
-        # --rm \
       docker run \
         --privileged \
+        --rm \
         --name $docker_name \
         --mount type=bind,source=$this_repo_root,target=$container_afl_template_path \
         --mount type=bind,source=$fuzz_session_root,target=$fuzz_session_root \
@@ -234,6 +234,7 @@ case $afl_mode in
             for i in {1..30}; do printf '.'; sleep 1; done
           "
 
+    printf "\n"
     sudo sysctl -w kernel.unprivileged_userns_clone=0
     ;;
 
